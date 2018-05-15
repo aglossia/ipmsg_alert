b552f5f13a86f1f0cc49660495880358cde436f4
aglossia <36531216+aglossia@users.noreply.github.com>
2018-05-15 20:47:28 +0900

    v1.4

---
+++ ipmsg_alert/setting/func.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using System.IO;
+
+namespace ipmsg_alert.setting
+{
+    class func
+    {
+        public string GetScreenSaverName()
+        {
+            string ret = "";
+
+            //キーを読み取り専用で開く
+            RegistryKey regkey = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", false);
+            //キーが存在しないときは null が返される
+            if (regkey == null) return "error";
+
+            string ssFilePath = (string)regkey.GetValue("SCRNSAVE.EXE");
+
+            string tmp = Path.GetExtension(ssFilePath);
+
+            if(Path.GetExtension(ssFilePath) == ".exe")
+            {
+                ret = Path.GetFileNameWithoutExtension(ssFilePath);
+            }else
+            {
+                ret = Path.GetFileName(ssFilePath);
+            }
+            return ret;
+        }
+    }
+}
